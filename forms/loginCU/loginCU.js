5c3241daadeb52aaa1cb0214be2cf45d8a080757
rww04096 <60627183+rww04096@users.noreply.github.com>
2020-04-05 13:45:09 -0500

    Login 1

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,3 +1,18 @@
+req =
 
+btnFavFoods.onclick=function(){
+  ChangeForm(favFoods)
+}
 
-
+btnLogin.onclick=function(){
+  req = Ajax("http://radlab.creighton.egu/appstudio/authLDAP.php", "POST", "j_username=" + inptNetID.value) 
+  
+  if (req.status == 200) {
+    lblFailure.hidden = false
+    lblFailure.value = "Your LDAP return code was " + req.responseText
+    ChangeForm(favFoods)
+ } else {
+    lblFailure.hidden = false
+    lblFailure.value = "Error: " + req.status
+  }
+}
