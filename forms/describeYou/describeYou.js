9b8d3696260c62a73e1f3a0b97a66e498106a2ef
rww04096 <60627183+rww04096@users.noreply.github.com>
2020-04-05 16:17:47 -0500

    dy1

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
 
+btnFavExercises.onclick=function(){
+  ChangeForm(favExercises)
+}
 
+rdoPersonality.onclick=function(){
 
-
-
+   lblReturn.textContent = (`I agree- you are a ${$("input[name=rdoPersonality]:checked").prop("value")} person`)
+}
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,11 +2,20 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+selExercises.clear()   
+   
+    for (i = 0; i <= exercises.length - 1; i++) 
+        selExercises.addItem(exercises[i])
 }
 
-selExercises.onfocusout=function(){
-
+btnSubmit1.onclick=function(){
+  let message = " " 
+  for (i = 0; i <= selExercises.text.length - 1; i++)
+     message = """ + selExercises.text[i] + """ + ", "
+  
+  if message = coreExercises {
+    NSB.MsgBox("You chose the situps and planks - those are the two core exercises")
+  } else {
+    NSB.MsgBox("You did not pick the two core exercises")
+  }
 }
-
-
